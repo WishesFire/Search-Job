dc4d1ad168b23d9767ba592d53840cd9b2357fda
liqzUISG <vmaksimus706@gmail.com>
2021-11-18 12:13:57 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="epamProject",
+    version="1.0",
+    author="Maksym Vojtsitskiy",
+    author_email="vmaksimus706@gmail.com",
+    description="Flask project for EPAM",
+    url="https://github.com/sa1am8/Graduation_Work",
+    packages=find_packages(),
+    python_requires='>=3.9'
+)
