84e2e5381ce0e882e98d39e8eb236a5894089e80
liqzUISG <vmaksimus706@gmail.com>
2021-11-21 16:49:45 +0200

    add start tests

--- app/configs/config.py
+++ app/configs/config.py
@@ -2,6 +2,7 @@
 
 class TestBaseConfig(object):
     SECRET_KEY = "osdjgposdfjfpo234po23jkpofsdj"
+    TESTING = True
     DEBUG = True
 
 
---
+++ app/tests/test_views.py
@@ -0,0 +1,16 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_status(client):
+    res = client.get("/")
+    assert 200 == res.status
+
