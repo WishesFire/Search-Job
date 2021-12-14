04b85df571b6a6590299e637e192fe967bde72c9
liqzUISG <vmaksimus706@gmail.com>
2021-12-14 13:43:26 +0200

    ref test categories

--- app/tests/test_categories.py
+++ app/tests/test_categories.py
@@ -1,40 +1,15 @@
-import pytest
 from app import create_app
 from app import db
 from app.models.model import Category
-from app.models.handlers import init_start_categories
 
 app = create_app()
+with app.app_context():
+    db.create_all()
 
 
-@pytest.fixture
-def created_test_db():
-    """
-    Create app context for db operations
-    """
-    app.config["TESTING"] = True
-    with app.app_context() as created_db:
-        db.create_all()
-        yield created_db
-
-
-def test_integrity_category_check(created_test_db):
-    """
-    Check element in category model
-    :param created_test_db: app context
-    """
-    with app.app_context():
-        result = Category.query.get(1)
-        if not result:
-            init_start_categories()
-            result = Category.query.get(1)
-    assert result.name == "Designer"
-
-
-def test_add_category(created_test_db):
+def test_add_category():
     """
     Check add element category in database
-    :param created_test_db: app context
     """
     category_example = "Testing"
 
@@ -53,10 +28,20 @@ def test_add_category(created_test_db):
         Category.query.filter_by(name=category_example).delete()
 
 
-def test_get_vacancies_from_category(created_test_db):
+def test_category_slug():
     """
-    Check vacancies with tied to category
-    :param created_test_db: app context
+    Check slug element category in database
     """
-    pass
+    category_example = "Testing"
 
+    with app.app_context():
+        check_exists = Category.query.filter_by(name=category_example).first()
+        if not check_exists:
+            new_category = Category(name=category_example)
+            db.session.add(new_category)
+            db.session.commit()
+
+        result = Category.query.filter_by(name=category_example).first()
+        assert result.slug
+
+        Category.query.filter_by(name=category_example).delete()
