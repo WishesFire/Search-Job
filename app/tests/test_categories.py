724cb138634a49a02734004134bb81ddc2ed4b96
liqzUISG <vmaksimus706@gmail.com>
2021-12-14 14:02:00 +0200

    ref

--- app/tests/test_categories.py
+++ app/tests/test_categories.py
@@ -1,18 +1,29 @@
-from app import create_app, db
+import pytest
+from app import db, create_app
 from app.models.model import Category
+
 
 app = create_app()
 
 
-def test_add_category():
+@pytest.fixture
+def created_test_db():
+    """
+    Create app context for db operations
+    """
+    app.config["TESTING"] = True
+    with app.app_context() as created_db:
+        db.create_all()
+        yield created_db
+
+
+def test_add_category(created_test_db):
     """
     Check add element category in database
     """
     category_example = "Testing"
 
     with app.app_context():
-        db.create_all()
-
         check_exists = Category.query.filter_by(name=category_example).first()
         if check_exists:
             Category.query.filter_by(name=category_example).delete()
@@ -27,15 +38,13 @@ def test_add_category():
         Category.query.filter_by(name=category_example).delete()
 
 
-def test_category_slug():
+def test_category_slug(created_test_db):
     """
     Check slug element category in database
     """
     category_example = "Testing"
 
     with app.app_context():
-        db.create_all()
-
         check_exists = Category.query.filter_by(name=category_example).first()
         if not check_exists:
             new_category = Category(name=category_example)
