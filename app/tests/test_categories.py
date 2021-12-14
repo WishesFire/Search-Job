2beed8b71b47e14181516edcb97cc9a795cc5e50
liqzUISG <vmaksimus706@gmail.com>
2021-12-14 13:49:51 +0200

    ref

--- app/tests/test_categories.py
+++ app/tests/test_categories.py
@@ -1,10 +1,7 @@
-from app import create_app
-from app import db
+from app import create_app, db
 from app.models.model import Category
 
 app = create_app()
-with app.app_context():
-    db.create_all()
 
 
 def test_add_category():
@@ -14,6 +11,8 @@ def test_add_category():
     category_example = "Testing"
 
     with app.app_context():
+        db.create_all()
+
         check_exists = Category.query.filter_by(name=category_example).first()
         if check_exists:
             Category.query.filter_by(name=category_example).delete()
@@ -35,6 +34,8 @@ def test_category_slug():
     category_example = "Testing"
 
     with app.app_context():
+        db.create_all()
+
         check_exists = Category.query.filter_by(name=category_example).first()
         if not check_exists:
             new_category = Category(name=category_example)
