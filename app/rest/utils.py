2afa604805e47637e3bc5ca88aa2151c9a785f29
liqzUISG <vmaksimus706@gmail.com>
2022-01-12 16:24:14 +0200

    add decorator for errors

---
+++ app/rest/utils.py
@@ -0,0 +1,14 @@
+from sqlalchemy import exc
+
+
+def vacancy_check(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except exc.ArgumentError:
+            return {"msg": "Invalid or conflicting function argument is supplied"}, 404
+        except exc.SQLAlchemyError:
+            return {"msg": "Execution of a database operation fails"}, 404
+        except Exception as error:
+            return {"msg": str(error)}, 404
+    return wrapper
--- app/rest/vacancies_api.py
+++ app/rest/vacancies_api.py
@@ -7,14 +7,15 @@ from flask_restful import Resource
 from app import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models.model import Vacancy, Category
-from sqlalchemy import exc
 from app.rest.serializers import vacancies_schema
 from .handlers import vacancy_post_args, vacancy_delete_args, vacancy_put_args, vacancy_get_args
 from app.service.validartors import VacancyFormValidator
+from .utils import vacancy_check
 
 
 class VacancyAPI(Resource):
     @classmethod
+    @vacancy_check
     def get(cls, category_slug):
         """
         Get all vacancies by category
@@ -23,28 +24,20 @@ class VacancyAPI(Resource):
         :return: json
         """
         args = vacancy_get_args.parse_args()
-        try:
-            category = Category.query.filter_by(slug=category_slug).first()
-            if args.get("filterSalary"):
-                filter_salary = float(args.get("filterSalary"))
-                all_vacancies = Vacancy.query.filter_by(category=category.id).filter(
-                            Vacancy.salary <= filter_salary).all()
-                if all_vacancies is []:
-                    return {"msg": "No vacancies were found for this filter"}, 200
-            else:
-                all_vacancies = Vacancy.query.filter_by(category=category.id).all()
-
-            vacancies_serialize = vacancies_schema.dump(all_vacancies)
-            return vacancies_serialize, 200
-
-        except exc.ArgumentError:
-            return {"msg": "Invalid or conflicting function argument is supplied"}, 404
-        except exc.SQLAlchemyError:
-            return {"msg": "Execution of a database operation fails"}, 404
-        except Exception as error:
-            return {"msg": error}, 404
+        category = Category.query.filter_by(slug=category_slug).first()
+        if args.get("filterSalary"):
+            filter_salary = float(args.get("filterSalary"))
+            all_vacancies = Vacancy.query.filter_by(category=category.id).filter(
+                        Vacancy.salary <= filter_salary).all()
+            if all_vacancies is []:
+                return {"msg": "No vacancies were found for this filter"}, 200
+        else:
+            all_vacancies = Vacancy.query.filter_by(category=category.id).all()
+        vacancies_serialize = vacancies_schema.dump(all_vacancies)
+        return vacancies_serialize, 200
 
     @classmethod
+    @vacancy_check
     @jwt_required()
     def post(cls, category_slug):
         """
@@ -53,38 +46,31 @@ class VacancyAPI(Resource):
         :param category_slug: category slug
         :return: json
         """
-        try:
-            user_id = get_jwt_identity()
+        user_id = get_jwt_identity()
 
-            args = vacancy_post_args.parse_args()
-            name = args.get("name")
-            salary = args.get("salary")
-            about = args.get("about")
-            contacts = args.get("contacts")
+        args = vacancy_post_args.parse_args()
+        name = args.get("name")
+        salary = args.get("salary")
+        about = args.get("about")
+        contacts = args.get("contacts")
 
-            validator = VacancyFormValidator(name, salary, about, contacts, category_slug)
-            vacancy_name = validator.check_name()
-            vacancy_salary = validator.check_salary()
-            category = validator.check_category()
+        validator = VacancyFormValidator(name, salary, about, contacts, category_slug)
+        vacancy_name = validator.check_name()
+        vacancy_salary = validator.check_salary()
+        category = validator.check_category()
 
-            if category:
-                new_vacancy = Vacancy(name=vacancy_name, salary=vacancy_salary, info=about,
-                                      contacts=contacts, user=user_id, category=category.id)
-                db.session.add(new_vacancy)
-                db.session.commit()
+        if category:
+            new_vacancy = Vacancy(name=vacancy_name, salary=vacancy_salary, info=about,
+                                  contacts=contacts, user=user_id, category=category.id)
+            db.session.add(new_vacancy)
+            db.session.commit()
 
-                return {"msg": "New vacancy successfully created", "id": new_vacancy.id}, 200
+            return {"msg": "New vacancy successfully created", "id": new_vacancy.id}, 200
 
-            return {"msg": "Category is not founded"}, 401
-
-        except exc.ArgumentError:
-            return {"msg": "Invalid or conflicting function argument is supplied"}, 404
-        except exc.SQLAlchemyError:
-            return {"msg": "Execution of a database operation fails"}, 404
-        except Exception as error:
-            return {"msg": error}, 404
+        return {"msg": "Category is not founded"}, 401
 
     @classmethod
+    @vacancy_check
     @jwt_required()
     def put(cls, category_slug):
         """
@@ -92,38 +78,31 @@ class VacancyAPI(Resource):
         Parameters: current_name, name, salary, about, contacts
         :return: json
         """
-        try:
-            user_id = get_jwt_identity()
-            args = vacancy_put_args.parse_args()
-            current_name = args.get("current_name")
-            name = args.get("name")
-            salary = args.get("salary")
-            about = args.get("about")
-            contacts = args.get("contacts")
-            vacancy = Vacancy.query.filter_by(name=current_name, user=user_id).first()
-            if vacancy:
-                if name:
-                    vacancy.name = name
-                elif salary:
-                    vacancy.salary = salary
-                elif about:
-                    vacancy.info = about
-                elif contacts:
-                    vacancy.contacts = contacts
-                db.session.commit()
+        user_id = get_jwt_identity()
+        args = vacancy_put_args.parse_args()
+        current_name = args.get("current_name")
+        name = args.get("name")
+        salary = args.get("salary")
+        about = args.get("about")
+        contacts = args.get("contacts")
+        vacancy = Vacancy.query.filter_by(name=current_name, user=user_id).first()
+        if vacancy:
+            if name:
+                vacancy.name = name
+            elif salary:
+                vacancy.salary = salary
+            elif about:
+                vacancy.info = about
+            elif contacts:
+                vacancy.contacts = contacts
+            db.session.commit()
 
-                return {"msg": "Vacancy successfully updated"}, 200
+            return {"msg": "Vacancy successfully updated"}, 200
 
-            return {"msg": "It's not your vacancy"}, 400
-
-        except exc.ArgumentError:
-            return {"msg": "Invalid or conflicting function argument is supplied"}, 404
-        except exc.SQLAlchemyError:
-            return {"msg": "Execution of a database operation fails"}, 404
-        except Exception as error:
-            return {"msg": error}, 404
+        return {"msg": "It's not your vacancy"}, 400
 
     @classmethod
+    @vacancy_check
     @jwt_required()
     def delete(cls, category_slug):
         """
@@ -131,18 +110,10 @@ class VacancyAPI(Resource):
         Parameters: name
         :return: json
         """
-        try:
-            user_id = get_jwt_identity()
-            args = vacancy_delete_args.parse_args()
-            name = args.get("name")
-            Vacancy.query.filter_by(name=name, user=user_id).delete()
-            db.session.commit()
+        user_id = get_jwt_identity()
+        args = vacancy_delete_args.parse_args()
+        name = args.get("name")
+        Vacancy.query.filter_by(name=name, user=user_id).delete()
+        db.session.commit()
 
-            return {"msg": f"Vacancy - {name} successfully deleted"}, 200
-
-        except exc.ArgumentError:
-            return {"msg": "Invalid or conflicting function argument is supplied"}, 404
-        except exc.SQLAlchemyError:
-            return {"msg": "Execution of a database operation fails"}, 404
-        except Exception as error:
-            return {"msg": error}, 404
+        return {"msg": f"Vacancy - {name} successfully deleted"}, 200
